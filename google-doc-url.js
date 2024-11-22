742038dfb05fd63af8497733050f1212a6f4ebe2
circolaremaremma <circolaremaremma@gmail.com>
2024-11-22 12:35:26 +0100

    Update google-doc-url.js - link a partner2

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WVJcQLcgPpF87193DTWSd06hXQi-Fe0pZlLN1NruuNI/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1rMc16kYn0lzwMyWGzDNnrhWRv4RU0vCtuzsDbFWA4Tw/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
